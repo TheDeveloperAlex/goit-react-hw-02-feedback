563127ee1f12cadfde9ea5d45b6d46656f68b228
TheDeveloperAlex <scorpion422@ukr.net>
2021-08-26 19:14:20 +0300

    Убирает лишний тайтл

--- src/Components/FeedbackOptions/FeedbackOptions.jsx
+++ src/Components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,7 +1,6 @@
 const FeedbackOptions = ({ good, neutral, bad }) => {
     return (
         <>
-            <h2>Leave feedback</h2>
             <div>
                 <button type="button" onClick={good }>good</button>
                 <button type="button" onClick={ neutral}>neutral</button> 
